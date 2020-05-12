ee00cc351a973b6cec1b46f623c94e0327d9d719
paulll <paulll@derpy.ru>
2020-05-12 23:11:18 +0300

    add project details view

--- views/pug.config.js
+++ views/pug.config.js
@@ -4,27 +4,24 @@
 const yaml = require('yaml');
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 const {categories} = yaml.parse(fs.readFileSync(__dirname + '/../projects/categories.yaml', {encoding: 'utf-8'}));
 
-const hl = 'en';
-const projectsByCategory = {}
 for (const category of categories) {
 	category.projects = fs.readdirSync(path.join(__dirname, `../projects`, category.path))
-		.map(x => yaml.parse(fs.readFileSync(path.join(__dirname, '../projects', category.path, x), {encoding: 'utf-8'})))
+		.map(x => Object.assign({filename: x}, yaml.parse(fs.readFileSync(path.join(__dirname, '../projects', category.path, x), {encoding: 'utf-8'}))))
 		.sort((a,b) => (b.awesomeness||0) - (a.awesomeness||0));
-
-	projectsByCategory[category.i18n[hl]] = [];
-	for (const project of category.projects) {
-		projectsByCategory[category.i18n[hl]].push(project.i11n[hl].name) 
-	}
+	category.projects
+		.forEach(x => x.id = crypto.createHash('sha256').update(category.path + x.filename).digest('hex').slice(0,16));
+	category.projects
+		.forEach(x => x.i18n.ru.description = x.i18n.ru.description.split('\n').map(x=>`<p>${x}</p>`).join(''))
+	category.projects
+		.forEach(x => x.i18n.en.description = x.i18n.en.description.split('\n').map(x=>`<p>${x}</p>`).join(''))
 }
-
-console.log(projectsByCategory, categories)
-
 
 module.exports = {
 	locals: {
-		projectsByCategory,
+		categories,
 		projectsByCategory_: {
 			'vk.com related': [
 				'graphite',
