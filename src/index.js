c02b6cffd6d35e210b79ff5f10576a3d7ccf661b
paulll <paulll@derpy.ru>
2020-05-18 01:02:21 +0300

    add an easter egg

--- src/index.js
+++ src/index.js
@@ -14,33 +14,42 @@ window.addEventListener('load', () => {
 	if (title) {
 		document.getElementsByTagName('title')[0].textContent = `${title} — paulll`;
 	}
+
+	/* hits / uniq hits calculation */
+	fetch('https://api.paulll.cc/landing/hit', {
+		method: 'POST',
+		mode: 'cors',
+		cache: 'no-cache',
+		body: JSON.stringify({
+			url: document.location.href,
+			title: document.querySelector(':target h2') ? document.querySelector(':target h2').textContent : 'paulll',
+			referrer: document.referrer,
+			client: {
+				languageInfo: [ allLangs, clientLang ],
+				userAgent: [ navigator.userAgent, navigator.appVersion ],
+				maxTouchPoints: navigator.maxTouchPoints,
+				cores: navigator.hardwareConcurrency, 
+				memory: navigator.deviceMemory,
+				dnt: navigator.doNotTrack,
+				connection: {
+					type: navigator.connection.effectiveType,  
+					rtt: navigator.connection.rtt, 
+					saveData: navigator.saveData, 
+					downlink: navigator.downlink,
+					maxDownlink: navigator.downlinkMax
+				},
+				platform: navigator.platform,
+				pixelRatio: window.devicePixelRatio
+			}
+		})
+	})
+	.then((response) => response.json())
+	.then((data) => {
+		if (data.you) {
+			document.getElementById('footer').textContent = `hello, ${data.you}`;
+		}
+	})
+	.catch(() => {/* i really don't care */})
 });
 
 
-/* hits / uniq hits calculation */
-fetch('https://api.paulll.cc/landing/hit', {
-	method: 'POST',
-	mode: 'cors',
-	cache: 'no-cache',
-	body: JSON.stringify({
-		url: document.location.href,
-		title: document.querySelector(':target h2') ? document.querySelector(':target h2').textContent : 'paulll',
-		client: {
-			languageInfo: [ allLangs, clientLang ],
-			userAgent: [ navigator.userAgent, navigator.appVersion ],
-			maxTouchPoints: navigator.maxTouchPoints,
-			cores: navigator.hardwareConcurrency, 
-			memory: navigator.deviceMemory,
-			dnt: navigator.doNotTrack,
-			connection: {
-				type: navigator.connection.effectiveType,  
-				rtt: navigator.connection.rtt, 
-				saveData: navigator.saveData, 
-				downlink: navigator.downlink,
-				maxDownlink: navigator.downlinkMax
-			},
-			platform: navigator.platform,
-			pixelRatio: window.devicePixelRatio
-		}
-	})
-}).catch(() => {/* i really don't care */});
